3dd4919b5a6a60e4ca3bf78d905ac3e9e869b420
unknown <wyyuddin@gmail.com>
2021-08-14 15:03:49 +0700

    Update Koneksi

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
  
-$query = mysql_query("select * from admin where username='$username' and password='$password'");
-$cek = mysql_num_rows($query);
+$query = mysqli_query($con, "select * from admin where username='$username' and password='$password'");
+$cek = mysqli_num_rows($query);
 echo $cek;
 ?>
